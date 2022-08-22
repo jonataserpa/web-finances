15cac790ab450408a1e3d0327f6d1ce4700aba61
Jonata <jonata.serpa@inatel.br>
2022-08-22 17:39:03 -0300

    feat(test): add registerform test

--- src/features/user/test/Table.spec.tsx
+++ src/features/user/test/Table.spec.tsx
@@ -21,6 +21,9 @@ const rowsMock: IUser[] = [
   },
 ];
 
+/**
+ * Not data table users
+ */
 test("not should render users table", () => {
   const { getByText } = render(
     <TableRows
@@ -32,6 +35,9 @@ test("not should render users table", () => {
   expect(getByText("No users")).toBeInTheDocument();
 });
 
+/**
+ * Users data table
+ */
 test("should render data users table", () => {
   const { getAllByTestId } = render(
     <TableRows
@@ -45,56 +51,4 @@ test("should render data users table", () => {
   );
   const fakeUsersTableRows = rowsMock.map((row) => row.name);
   expect(userTableRows).toEqual(fakeUsersTableRows);
-});
-
-describe("validate component VTextField", () => {
-  test("renders an input label", () => {
-    render(
-      <VTextField
-        name="name"
-        type="text"
-        label="Name label"
-        value="Test default"
-        error={undefined}
-        helperText="error message"
-      />
-    );
-
-    const label = screen.getByLabelText("Name label");
-    expect(label).toBeInTheDocument();
-  });
-
-  test("renders an input value", () => {
-    render(
-      <VTextField
-        name="name"
-        type="text"
-        label="Name label"
-        value="Test default"
-        error={undefined}
-        helperText=""
-      />
-    );
-
-    const value = screen.getByDisplayValue("Test default");
-    expect(value).toBeInTheDocument();
-  });
-
-  test("renders an error message section empty", () => {
-    render(
-      <VTextField
-        name="name"
-        type="text"
-        label="Name label"
-        value=""
-        error={false}
-        helperText="error message"
-        role="alert"
-      />
-    );
-
-    const alert = screen.getByRole("alert");
-    expect(alert).toBeInTheDocument();
-    expect(alert).toHaveTextContent("error message");
-  });
 });
