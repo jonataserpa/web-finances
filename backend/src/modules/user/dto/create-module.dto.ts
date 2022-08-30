62024ac57d399bbdc7be33cc03f9531df2f5c08b
Jonata <jonata.serpa@inatel.br>
2022-08-30 17:13:09 -0300

    add lib swagger

--- backend/src/modules/user/dto/create-module.dto.ts
+++ backend/src/modules/user/dto/create-module.dto.ts
@@ -1,1 +1,30 @@
-export class CreateModuleDto {}
+import {
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    Length,
+  } from 'class-validator';
+import constants from 'src/config/constants';
+export class CreateModuleDto {
+
+    // @IsOptional()
+    // id?: String;
+
+    // @Length(constants.STRING_MIN, constants.STRING_MAX)
+    // @IsString()
+    // name: String;
+
+    // @Length(constants.STRING_MIN, constants.STRING_MAX)
+    // @IsString()
+    // email: String;
+
+    // @Length(constants.NUMBER_MIN_PHONE)
+    // @IsString()
+    // phone_user: String;
+
+    // @IsString()
+    // company_id_user: String;
+
+    // @IsString()
+    // radiogender: String;
+}
